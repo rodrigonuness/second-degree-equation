3df9c1050672b2dff59a0438bc442b603d38bb7c
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-08-29 11:36:32 -0300

    Update second degree equation.py

--- second degree equation.py
+++ second degree equation.py
@@ -1,35 +1,35 @@
-thin: str = 'c'
-while(thin == 'c' or thin == 'C'):
+repeat: str = 'c'
+while(repeat == 'c' or repeat == 'C'):
     print('Ax² + Bx + C= 0\n')
     a = int(input('Type A: '))
     b = int(input('Type B: '))
     c = int(input('Type C: '))
 
-    j: str = 'oi0'
+    info: str = 'none'
 
     if (a != 0):
         if (c == 0):
-            j = 'incomplete'
+            info = 'incomplete'
         elif (b == 0):
-            j = 'incomplete'
+            info = 'incomplete'
         elif (b != 0 and c != 0):
-            j = 'complete'
+            info = 'complete'
     else:
-        j = 'do not exist'
+        info = 'do not exist'
 
     print(a, 'x² + ', b, 'x + ', c, '= 0\n')
 
-    print('is ', j)
+    print('is ', info)
 
-    al: float = (b ** 2) - (4 * a * c)
+    baske: float = (b ** 2) - (4 * a * c)
 
-    print('Delta is:', al)
-    if (al < 0):
+    print('Delta is:', baske)
+    if (baske < 0):
         print('There is no real root\n')
     else:
         print('There is real root: \n')
-    baske1 = -(b) + (al ** (1 / 2))
+    baske1 = -(b) + (baske ** (1 / 2))
     baske2 = 2 * a
     print(baske1, '/', baske2, '=')
     print('x = ±',baske1 / baske2)
-    thin = str(input('FOR CONTINUE PRESS C OR c, FOR OUT PRESS S ou s.........'))
+    repeat = str(input('FOR CONTINUE PRESS C OR c, FOR OUT PRESS S ou s.........'))
